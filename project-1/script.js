433b9f7c0ca04948acc97a9b7e410d9aec1be95d
nishantchaurasiya <nishantchaurasiya1108@gmail.com>
2025-06-30 08:54:15 +0530

    todo-app completed

--- project-1/script.js
+++
@@ -1,8 +0,0 @@
-const btns=document.querySelectorAll(".btn")
-const main=document.querySelector("main")
-btns.forEach((btn)=>{
-btn.addEventListener("click",()=>{
-    main.style.backgroundColor=btn.value;
-});
-
-});
